f4bb864f11cd85db0a6344bb92af4c415088cb60
empodi <kglein9513@gmail.com>
2023-11-15 13:25:20 +0900

    Initial commit

---
+++ src/main/java/com/service/spring/ScsaSnackApplication.java
@@ -0,0 +1,13 @@
+package com.service.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScsaSnackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScsaSnackApplication.class, args);
+    }
+
+}
