ce86cea0d757a5104b17acc47d8ed3023d6db70a
AHN Kwang Hwi <kanean99@naver.com>
2023-11-16 14:23:24 +0900

    history vo 수정
    .

--- src/main/java/com/service/spring/domain/History.java
+++ src/main/java/com/service/spring/domain/History.java
@@ -5,52 +5,68 @@ public class History {
     private String memberId;
     private String snackId;    
     private int count;
+    private String time;
     
-    public History() {
-    	
-    }
-	public History(String memberId, String snackId, int count) {
-		super();
-		this.memberId = memberId;
-		this.snackId = snackId;
-		this.count = count;
-	}
-	public History(Long historyId, String memberId, String snackId, int count) {
+    public History() {}
+    
+	public History(Long historyId, String memberId, String snackId, int count, String time) {
 		super();
 		this.historyId = historyId;
 		this.memberId = memberId;
 		this.snackId = snackId;
 		this.count = count;
+		this.time = time;
 	}
+
 	public Long getHistoryId() {
 		return historyId;
 	}
+
 	public void setHistoryId(Long historyId) {
 		this.historyId = historyId;
 	}
+
 	public String getMemberId() {
 		return memberId;
 	}
+
 	public void setMemberId(String memberId) {
 		this.memberId = memberId;
 	}
+
 	public String getSnackId() {
 		return snackId;
 	}
+
 	public void setSnackId(String snackId) {
 		this.snackId = snackId;
 	}
+
 	public int getCount() {
 		return count;
 	}
+
 	public void setCount(int count) {
 		this.count = count;
 	}
+
+	public String getTime() {
+		return time;
+	}
+
+	public void setTime(String time) {
+		this.time = time;
+	}
+
 	@Override
 	public String toString() {
 		return "History [historyId=" + historyId + ", memberId=" + memberId + ", snackId=" + snackId + ", count="
-				+ count + "]";
+				+ count + ", time=" + time + "]";
 	}
+	
+	
+	
+	
 	
 	
 }
