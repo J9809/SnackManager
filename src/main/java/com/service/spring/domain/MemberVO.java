6c5fbbb742734bf9ca9784544d69ef3911521002
J9809 <77200602+J9809@users.noreply.github.com>
2023-11-16 13:08:15 +0900

    MemberVO 생성
    MemberVO 파일 생성한 거 올립니다!

---
+++ src/main/java/com/service/spring/domain/MemberVO.java
@@ -0,0 +1,59 @@
+package com.service.spring.domain;
+
+public class MemberVO {
+    private String memberId;
+    private String password;
+    private String name;    
+    private String voteFlag;
+    private String role;
+	
+    public MemberVO() { }
+    
+    public MemberVO(String memberId, String password, String name, String voteFlag, String role) {
+		super();
+		this.memberId = memberId;
+		this.password = password;
+		this.name = name;
+		this.voteFlag = voteFlag;
+		this.role = role;
+	}
+    
+    public String getMemberId() {
+		return memberId;
+	}
+	public void setMemberId(String memberId) {
+		this.memberId = memberId;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getVoteFlag() {
+		return voteFlag;
+	}
+	public void setVoteFlag(String voteFlag) {
+		this.voteFlag = voteFlag;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+	@Override
+	public String toString() {
+		return "MemberVO [memberId=" + memberId + ", password=" + password + ", name=" + name + ", voteFlag=" + voteFlag
+				+ ", role=" + role + "]";
+	}
+	
+    
+    
+}
