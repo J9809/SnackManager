277a020c44cfdddd4b6a124631baa56851cacb28
AHN Kwang Hwi <kanean99@naver.com>
2023-11-16 13:25:44 +0900

    login 만드는중
    ...

--- src/test/java/com/service/spring/ScsaSnackApplicationTests.java
+++ src/test/java/com/service/spring/ScsaSnackApplicationTests.java
@@ -1,13 +1,30 @@
 package com.service.spring;
 
+import java.io.IOException;
+import java.io.Reader;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.service.spring.domain.MemberVO;
 
 @SpringBootTest
 class ScsaSnackApplicationTests {
 
     @Test
-    void contextLoads() {
+    void contextLoads() throws IOException {
+    	Reader r = Resources.getResourceAsReader("config/SqlMapConfig.xml");
+    	SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(r);
+	  
+		  //2. SqlSession -- SqlSessionTemplate
+	   SqlSession session=factory.openSession();
+	   MemberVO pvo = new MemberVO("muscleup15", "안광휘", "1234", "N", "student");
+	   MemberVO rvo = session.selectOne("SnackMapper.login", pvo);
+	   System.out.println(rvo);
     }
 
 }
